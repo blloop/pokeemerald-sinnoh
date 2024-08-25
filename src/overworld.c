eb25f21e24ff2aaf5583f1ed460e0d7d3553c0fe
blloop <42186044+blloop@users.noreply.github.com>
2024-08-25 13:30:21 -0700

    clean up player house objects + events, add basic text intro

--- src/overworld.c
+++ src/overworld.c
@@ -1547,7 +1547,7 @@ void CB2_NewGame(void)
     PlayTimeCounter_Start();
     ScriptContext_Init();
     UnlockPlayerFieldControls();
-    gFieldCallback = ExecuteTruckSequence;
+    // gFieldCallback = ExecuteTruckSequence;
     gFieldCallback2 = NULL;
     DoMapLoadLoop(&gMain.state);
     SetFieldVBlankCallback();
