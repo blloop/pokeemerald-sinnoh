906666468b46c7e8c5fd010f7d528ecfb8ce0f32
blloop <42186044+blloop@users.noreply.github.com>
2025-04-09 16:39:53 -0700

    update tiles, add graphics to lake verity

--- include/constants/global.h
+++ include/constants/global.h
@@ -156,7 +156,7 @@
 #define SIDEWAYS_STAIRS_IMPLEMENTED     TRUE
 #define POST_BATTLE_FOLLOWER_FIX        FALSE   //if you experience the follower de-syncing with the player after battle, set to TRUE
 
-#define SLOW_MOVEMENT_ON_STAIRS         FALSE
+#define SLOW_MOVEMENT_ON_STAIRS         TRUE
 #define FOLLOW_ME_IMPLEMENTED           TRUE    //for stairs movement. see ObjectMovingOnRockStairs in src/field_player_avatar.c
 
 #endif // GUARD_CONSTANTS_GLOBAL_H
